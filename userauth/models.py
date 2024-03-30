37f47e8ba1bfc1d519a422e1e756fc1141aed631
krown12 <kcsajan272@gmail.com>
2024-03-30 17:58:52 +0545

    authuser model

--- userauth/models.py
+++ userauth/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 # Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length = 100)
+    middle_name = models.CharField(max_length = 100,null=True,blank=True)
+    last_name = models.CharField(max_length = 100)
+    phone_number = models.CharField(max_length = 10,null=True,blank=True)
+    email = models.EmailField(max_length = 100,unique = True)
+    # username = models.CharField(max_length=150)
+    female = "F"
+    male ="M"
+    others ='O'
+    Gender =(
+        (female,"female"),
+        (male,"Male"),
+        (others,"others")
+    )
+    gender = models.CharField(max_length=50, choices = Gender,default = male)
+    DOB = models.DateField()
+    profile_picture = models.ImageField(upload_to='user_pfps/',null= True,blank=True)
+
+
+
+    USERNAME_FIELD = 'email'
+    
+    REQUIRED_FIELDS=[]
+
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    is_superuser = models.BooleanField(default = False)
+    is_normal_user = models.BooleanField(default = True)
+    
+    date_joined = models.DateTimeField(default = timezone.now)
--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from rest_framework.viewsets import ModelViewSet,generics
+from django.contrib.auth import get_user_model
+from .serializers import UserRegisterSerailizer,UserLoginSerializer
+from rest_framework.response import Response
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view 
 # Create your views here.
+user_obj = get_user_model()
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserRegisterSerailizer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    email = serializer.validated_data.get('email')
+    password = serializer.validated_data.get('password')
+    first_name = serializer.validated_data.get('first_name')
+    last_name = serializer.validated_data.get('last_name')
+    middle_name = serializer.validated_data.get('middle_name')
+    phone_number = serializer.validated_data.get('phone_number')
+    gender = serializer.validated_data.get('gender')
+    DOB = serializer.validated_data.get('DOB')
+    
+    user = user_obj.objects.create(email=email,
+                                    password=password,
+                                    first_name=first_name,
+                                    last_name=last_name,
+                                    middle_name=middle_name,
+                                    phone_number=phone_number,
+                                    gender=gender,
+                                    DOB=DOB)
+
+    if user:
+       
+        return Response('user has been created')
+    return Response("something went wrong")
+    
+
+@api_view(['POST'])
+def login(request):
+    data = request.data
+    email = data.get('email')
+    password = data.get('password')
+    user = authenticate(username = email,password=password)
+    if user:
+        token,_ = Token.objects.get_or_create(user=user)
+        return Response({
+            "username":user.get_username(),
+            "token":token.key,
+        })
+    return  Response(user)
